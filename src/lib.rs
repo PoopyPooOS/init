c24520e92b0980e6e73bd4aaa6f6b044bc28d78c
altacountbabi <radustefan379@gmail.com>
2024-06-05 12:11:32 +0300

    Added support for environment variables and mounting pseudo filesystems.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::{thread, time::Duration};
 
 pub fn infinite_loop() -> ! {
-    loop { thread::sleep(Duration::from_secs(u64::MAX)) }
+    loop {
+        thread::sleep(Duration::from_secs(u64::MAX))
+    }
 }
