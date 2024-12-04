e23c093c1a77369b0c131488eaa5e6cc3b8122d6
altacountbabi <altacountbabi@users.noreply.github.com>
2024-12-04 13:35:03 +0200

    Use new system config location.

--- src/config.rs
+++ src/config.rs
@@ -11,9 +11,9 @@ pub struct PartialConfig {
 
 pub async fn read() -> Result<PartialConfig, Box<Log>> {
     match eval::<PartialConfig>(
-        fs::read_to_string("/system/config.tl")
-            .await
-            .map_err(|_| Box::new(make_fatal!("Failed to read config file", hint: "Check if /system/config.tl exists")))?,
+        fs::read_to_string("/config/system.tl").await.map_err(|err| {
+            Box::new(make_fatal!(format!("Failed to read config file: {err}"), hint: "Check if /system/config.tl exists"))
+        })?,
     )? {
         Some(config) => Ok(config),
         None => Err(Box::new(
